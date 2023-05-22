21f90858d169dff2333387e105e0b2a2bccab7cf
Prashu Pandey <prashupandey0210@gmail.com>
2023-05-22 22:08:32 +0530

    22/05/2023

--- app/src/main/java/com/example/taxi/Create.kt
+++ app/src/main/java/com/example/taxi/Create.kt
@@ -3,11 +3,16 @@ package com.example.taxi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Create : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        val create=findViewById<Button>(R.id.bt_create)
+        val find=findViewById<Button>(R.id.bt_find)
+
 
         create?.setOnClickListener{
             val intent = Intent(this,ChoosePartner::class.java)
