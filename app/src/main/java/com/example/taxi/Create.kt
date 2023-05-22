bdc443b75e996814bda4fca1803f54a643a77e63
Prashu Pandey <prashupandey0210@gmail.com>
2023-05-22 21:58:24 +0530

    22/05/2023

--- app/src/main/java/com/example/taxi/Create.kt
+++ app/src/main/java/com/example/taxi/Create.kt
@@ -1,5 +1,6 @@
 package com.example.taxi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,17 @@ class Create : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        create?.setOnClickListener{
+            val intent = Intent(this,ChoosePartner::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        find?.setOnClickListener{
+            val intent = Intent(this,ChoosePartner::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
